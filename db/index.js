f767cb847a4a6467b3c406388ca6e2a151058a79
Rebecca Carr <reca9238@colorado.edu>
2021-12-04 10:49:33 -0700

    added viewProducts method

--- db/index.js
+++ db/index.js
@@ -6,6 +6,15 @@ const client = new Client(DB_URL);
 
 // database methods
 
+async function getAllProducts()
+{
+  const {rows} = await client.query(
+    `SELECT *
+    FROM products;`)
+
+    return rows;
+}
+
 // export
 module.exports = {
   client,
