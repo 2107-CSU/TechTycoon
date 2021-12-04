f62db60f94ba332540c617bd102019902e8bcbfd
JordisonMatthew <jordison.m@hotmail.com>
2021-12-04 11:37:48 -0700

    added edit product db adapter function

--- db/index.js
+++ db/index.js
@@ -5,9 +5,28 @@ const DB_URL = process.env.DATABASE_URL || `postgres://localhost:5432/${ DB_NAME
 const client = new Client(DB_URL);
 
 // database methods
+async function editProduct(forms = {}) {
+  const { id } = fields;
+  delete fields.id;
 
+  const setString = Object.keys(fields).map((key, idx)
+    `"${key}"=$${index + 1}`).join(', ');
+
+    try {
+      const {rows: [product] } = await client.query(`
+      UPDATE products
+      SET ${setString}
+      WHERE id=${id}
+      RETURNING *;`, [Object.values(fields)]);
+
+      return product;
+    } catch (error) {
+      throw error
+    }
+}
 // export
 module.exports = {
   client,
+  editProduct
   // db methods
 }
