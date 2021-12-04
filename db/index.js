5214892ade33648587f306d398ffcf0e5e859b26
Maureen <mlincke15@gmail.com>
2021-12-04 11:57:10 -0700

    changing db/index.js structure

--- db/index.js
+++ db/index.js
@@ -5,6 +5,16 @@ const DB_URL = process.env.DATABASE_URL || `postgres://localhost:5432/${ DB_NAME
 const client = new Client(DB_URL);
 
 // database methods
+//-----------------PRODUCT METHODS-----------------------//
+async function getAllProducts()
+{
+  const {rows} = await client.query(
+    `SELECT *
+    FROM products;`)
+
+    return rows;
+}
+
 async function addProduct({ name, description, price, photo, availability, quantity }){
   try {
     const {rows} = await client.query(`
@@ -17,15 +27,6 @@ async function addProduct({ name, description, price, photo, availability, quant
   } catch (error) {
     throw error;
   }
-}
-
-async function getAllProducts()
-{
-  const {rows} = await client.query(
-    `SELECT *
-    FROM products;`)
-
-    return rows;
 }
 
 // export
