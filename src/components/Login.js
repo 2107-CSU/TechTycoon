0e0dd7d6ea2d187417af821cf38d773483fd104e
JordisonMatthew <jordison.m@hotmail.com>
2021-12-11 13:01:15 -0700

    added react html to Login component and should now be a functioning login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,95 @@
 import React, {useState, useEffect} from "react";
+import { Link } from 'react-router-dom';
+import {login, register} from '../api/index';
 
-const Login = () => {
-    return <h1> Login </h1>
+const Login = ({match, history, setToken}) => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [email, setEmail] = useState('');
+
+    return (
+        <>
+            {match.url === '/login'?
+                <h1>Login</h1>
+                :
+                <h1>Register</h1>
+            }
+            <form 
+            onSubmit={async (event) => {
+                event.preventDefault();
+
+                if (match.url === '/register') {
+                    // check password and confirm password
+                    if (password !== confirmPassword) {
+                        alert('password and confirm password must match');
+                        return;
+                    }
+
+                    // attempting to register the user
+                    const result = await register(username, password, email);
+
+                    // lets the user know if their password is too short
+                    if (result === "Password must be 8 or more characters") alert(result);
+                    else {
+                        alert('registered successfully please login to continue');
+                        history.push('/login');
+                    }
+                } else {
+                    // attempting to log the user in
+                    const result = await login(username, password);
+
+                    if (result.name) alert(result.message); // missing username or password
+                    else if (result === 'Your password is incorrect!') alert(result) // incorrect passsword
+                    else {
+                        // successful login
+                        setToken(result.token);
+                        localStorage.setItem('token', result.token);
+                        alert(result.message);
+                    }
+                }
+            }} >
+                <input 
+                value={username}
+                placeholder='username'
+                onChange={(event) => {
+                    setUsername(event.target.value);
+                }} />
+                <input 
+                value={password}
+                placeholder='password'
+                onChange={(event) => {
+                    setPassword(event.target.value);
+                }} />
+                {match.url === '/register'?
+                    <>
+                        <input 
+                        value={confirmPassword}
+                        placeholder='confirm password'
+                        onChange={(event) => {
+                            setConfirmPassword(event.target.value);
+                        }} />
+
+                        <input 
+                        value={email}
+                        placeholder='email'
+                        onChange={(event) => {
+                            setEmail(event.target.value);
+                        }} />
+                    </>
+                    :
+                    null
+                }
+                <button type='submit'>Submit</button>
+                { // Adjust the link displayed to the user depending on the page they are on
+                    match.url === '/register'?
+                        <Link to='/login'>Already have an account?</Link>
+                        :
+                        <Link to='/register'>Don't have an account?</Link>
+                }
+            </form>
+        </>
+    )
 }
 
 export default Login;
