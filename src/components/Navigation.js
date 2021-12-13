6bac5a0ff86a7edcb559ce66c4a376b02b52a8bd
JordisonMatthew <jordison.m@hotmail.com>
2021-12-12 18:17:41 -0700

    added logout functionality to the Navigation component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from "react";
 import { Link } from "react-router-dom";
 
-const Navigation = ({token}) => {
+const Navigation = ({token, setToken}) => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <h1 className="navbar-brand">Tech Tycoons</h1>
@@ -10,7 +10,20 @@ const Navigation = ({token}) => {
                     <li className="nav-item"><Link to='/cart' className="nav-link">Cart</Link></li>
                     <li className="active" className="nav-item active"><Link to='/products' className="nav-link">Products</Link></li>
                     {!!token ? 
-                    <li className="nav-item"><Link to='/profile' className="nav-link">Profile</Link></li> : 
+                    <>
+                    <li className="nav-item"><Link to='/profile' className="nav-link">Profile</Link></li> 
+                    <li className="nav-item">
+                        <Link 
+                            to='/login' 
+                            className="nav-link"
+                            onClick={() => {
+                                setToken('');
+                                localStorage.removeItem('token');
+                            }}
+                        >Logout</Link> 
+                    </li>
+                    </>
+                    : 
                     <li className="nav-item"><Link to='/login' className="nav-link">Login</Link></li>}
                 </ul>
             </div>
