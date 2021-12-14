1c06b5b8ad3a06dbd49600fa92f88ef8826297b3
Rebecca Carr <reca9238@colorado.edu>
2021-12-14 14:21:49 -0700

    added order products to profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,13 +1,18 @@
 import React, {useState, useEffect} from "react";
-import { getOrdersByUser } from "../api";
+import { getOrdersByUser, getProductsByOrder } from "../api";
 
 const Profile = ({token}) => {
     const [orders, setOrders] = useState([]);
+    const [orderProducts, setOrderProducts] = useState([]);
     useEffect(() => {
         const fetchData = async () => {
           if(token){
             const orders = await getOrdersByUser(token);
-            setOrders(orders);
+            console.log(orders);
+            await setOrders(orders);
+            const allOrderProducts = await Promise.all(orders.map(order => getProductsByOrder(token, order.id)));
+            console.log(allOrderProducts);
+            setOrderProducts(allOrderProducts);
           }
         }
         fetchData();
@@ -18,9 +23,18 @@ const Profile = ({token}) => {
             <h1> Profile </h1>
             {token ? 
             <div>
+                <h2>Orders: </h2>
                 {orders.map((order, indx) => {
                     return<div key = {indx}>
-                        <h3>{order.id}</h3>
+                        <h3>Order {indx} created {order.date}</h3>
+                        <h4>Products: </h4>
+                        {orderProducts.length === orders.length ? <div>
+                        {orderProducts[indx].map((product, indx) => {
+                            return<div key = {indx}>
+                                <h5>{product.name}</h5>
+                                <p>Quantity: {product.quantity}</p>
+                            </div>
+                        })} </div> : null}
                     </div>
                 })}
             </div>:null}
