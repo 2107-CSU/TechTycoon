5f7f0da2ae983a5616d17fe9f1e3fc76129cfbbc
JordisonMatthew <jordison.m@hotmail.com>
2021-12-14 21:49:33 -0700

    moved fetch functions into their correct files, this api/index will now serve as an export folder for all our fetch functions

--- src/api/index.js
+++ src/api/index.js
@@ -1,68 +1,6 @@
 import axios from 'axios';  // makes calls to the api, api reuqests
 import {register, login} from './users';
 import { createOrder } from './orders';
-const BaseUrl = "http://localhost:5000/";
-
-
-export async function getSingleProduct(productId)
-{
-  try{
-    const response = await fetch(BaseUrl + 'api/products/' + productId, {
-      headers: {
-        'Content-Type': 'application/json',
-      }
-    })
-    const product = await response.json();
-    console.log(product)
-    return product;
-
-  } catch(error)
-  {
-    console.log(error)
-    throw error;
-  }
-}
-
-export async function getOrdersByUser(token)
-{
-  try{
-    const response = await fetch(BaseUrl + 'api/orders/userOrders', {
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + token
-    }
-    })
-      const orders = await response.json();
-      return orders;
-
-  } catch(error)
-  {
-    console.log(error);
-    throw error;
-  }
-}
-
-export async function getProductsByOrder(token, orderId)
-{
-  try{
-    const response = await fetch(BaseUrl + 'api/orderproducts/' + orderId, {
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + token
-      }
-    })
-    const orderProducts = await response.json();
-    return orderProducts;
-
-  } catch(error)
-  {
-    console.log(error);
-    throw error;
-  }
-}
-
-// export async function 
-
 
 export async function getSomething() {
   try {
