7c94c129790f40358b13a649397cdf41f60990df
Rebecca Carr <reca9238@colorado.edu>
2021-12-22 19:28:16 -0700

    added styling to cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,6 +1,9 @@
 import React, {useState, useEffect} from "react";
 import {  removeProductFromCart, calculateCartPrice} from "./functions";
 import { createOrder, addProductToOrder } from "../api";
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 
 const Cart = ({token, cart, setCart}) => {
@@ -11,13 +14,19 @@ const Cart = ({token, cart, setCart}) => {
     }, []);
     return <div>
        <h1> Cart </h1> 
+       <Row xs={1} md={2} className="g-4">
        {cart.map((productObj, indx) => {
          const {product, quantity} = productObj;
-         return <div key = {indx} >
-                <h3>{product.name}</h3>
-                <p>{product.description}</p>
-                <p>price: {product.price}</p>
-                <p>quantity: {quantity}</p>
+         return <Col key = {indx} >
+                <Card style={{ width: '18rem' }}>
+                <Card.Header>{product.categories}</Card.Header>
+                <Card.Img src={`photos/${product.photo}.jpg`} variant='top'/>
+                <Card.Body>
+                  <Card.Title>{product.name}</Card.Title>
+                  <Card.Text>{product.description}</Card.Text>
+                  <Card.Text>price: {product.price}</Card.Text>
+                  <Card.Text>quantity: {quantity}</Card.Text>
+                </Card.Body>
            <form>
              <input type = "text" 
              placeholder= "New Quantity"
@@ -36,8 +45,10 @@ const Cart = ({token, cart, setCart}) => {
 
             }}
            >Remove product from Cart</button>
-           </div>
+           </Card>
+           </Col>
        })}
+       </Row>
        <p>Total Price: ${calculateCartPrice(cart)}</p>
        {token && cart.length? <button
         onClick = {async () => {
