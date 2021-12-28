5ab1f22c2b6a598db2aefaf2e8157b239d573ca1
Maureen <mlincke15@gmail.com>
2021-12-28 15:31:53 -0700

    adding stripepromise

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,8 @@ import {loadStripe} from '@stripe/stripe-js';
 import { Elements } from '@stripe/react-stripe-js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const stripePromise = loadStripe('pk_test_51K6JdyKsoRJpj5LyBuDfgXdryocGnfkLuxrRm12ZQhsPuWAjlcnpGJPPimIgVfwDeZ0Nl4WfX5970NH6dgI4whq600h4VIo3dH');
+
 const App = () => {
   const [products, setProducts] = useState([]);
   const [token, setToken] = useState(localStorage.getItem('token'));
