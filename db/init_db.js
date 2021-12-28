fa0844ec1b033a749f4d5b8985430518d4b7bde0
JordisonMatthew <jordison.m@hotmail.com>
2021-12-28 12:38:33 -0700

    added isActive as a variable to the users table

--- db/init_db.js
+++ db/init_db.js
@@ -74,7 +74,8 @@ async function buildTables() {
         username VARCHAR(255) UNIQUE NOT NULL,
         password VARCHAR(255) NOT NULL,
         email VARCHAR(255) UNIQUE NOT NULL,
-        "isAdmin" BOOLEAN DEFAULT false
+        "isAdmin" BOOLEAN DEFAULT false,
+        "isActive" BOOLEAN DEFAULT true
       );
       CREATE TABLE reviews(
         id SERIAL PRIMARY KEY,
